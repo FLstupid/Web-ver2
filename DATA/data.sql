5ea605d9bdc32417ce275a00933883438316077f
FLstupid <61163065+FLstupid@users.noreply.github.com>
2021-10-12 08:25:35 +0700

    Create data.sql

---
+++ DATA/data.sql
@@ -0,0 +1,5 @@
+use dhs;
+
+insert into account values (1,'minhnhut','123',0374062236,'19110257@student.hcmute.edu.vn',null,'20010527',0,current_timestamp());
+insert into account values (2,'minhduc','123',0374062236,'19110257@student.hcmute.edu.vn','MD','20010527',1,current_timestamp());
+insert into account values (3,'minhhoang','123',0374062236,'19110257@student.hcmute.edu.vn','MH','20010527',1,current_timestamp());
